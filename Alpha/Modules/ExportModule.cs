6faa58e9824066330e8cc6a330d208a86c15d9a8
NotNite <hi@notnite.com>
2023-05-15 16:34:42 -0400

    add bulk exports

--- Alpha/Modules/ExportModule.cs
+++ Alpha/Modules/ExportModule.cs
@@ -2,6 +2,7 @@
 using Alpha.Core;
 using ImGuiNET;
 using NativeFileDialogSharp;
+using Serilog;
 
 namespace Alpha.Modules;
 
@@ -11,10 +12,36 @@ public class ExportModule : SimpleModule {
 
     public ExportModule() : base("File Export", "Data") { }
 
-    internal override void SimpleDraw() {
-        ImGui.InputText("Path", ref this._path, 1024);
+    private void BulkExport(string[] lines, string outDir) {
+        foreach (var line in lines) {
+            try {
+                var file = Services.GameData.GetFile(line);
+                var dir = Path.GetDirectoryName(line);
+                var name = Path.GetFileName(line);
+                
+                Directory.CreateDirectory(Path.Combine(outDir, dir));
+                File.WriteAllBytes(
+                    Path.Combine(outDir, dir, name),
+                    file.Data
+                );
+            } catch (Exception e) {
+                Log.Error(e, "Failed to export file {Path}", line);
+            }
+        }
+    }
 
+    internal override void SimpleDraw() {
         if (ImGui.Button("Export file")) {
+            var lines = this._path.Trim().Split("\n");
+            if (lines.Length > 1) {
+                var dir = Dialog.FolderPicker();
+                if (dir?.Path is not null) {
+                    this.BulkExport(lines, dir.Path);
+                }
+
+                return;
+            }
+
             var file = Services.GameData.GetFile(this._path);
 
             if (file is null) {
@@ -37,5 +64,7 @@ public class ExportModule : SimpleModule {
         if (this._errored) {
             ImGui.TextColored(new Vector4(1, 0, 0, 1), "Couldn't save file - maybe it doesn't exist?");
         }
+
+        ImGui.InputTextMultiline("Paths", ref this._path, 1024, ImGui.GetContentRegionAvail());
     }
 }
